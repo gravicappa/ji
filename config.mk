2cceb48f9c3f40a3a33218cf7293c8533d807fa5
Ramil Farkhshatov <ramil@gmx.co.uk>
2010-04-17 19:37:55 +0400

    Linking against gnutls.

--- config.mk
+++ config.mk
@@ -6,8 +6,11 @@ man1dir = ${mandir}/man1
 destdir = 
 version = 0.1
 
-INCLUDES = ${shell pkg-config --cflags iksemel}
-LIBS = -lutil ${shell pkg-config --libs iksemel}
+INCLUDES = ${shell pkg-config --cflags gnutls} \
+           ${shell pkg-config --cflags iksemel}
+LIBS = -lutil \
+       ${shell pkg-config --libs gnutls} \
+       ${shell pkg-config --libs iksemel}
 
-CFLAGS = -O0 -g -pedantic -W -Wall -DVERSION=\"${version}\" ${INCLUDES}
+CFLAGS = -O0 -g -pedantic -Wall -DVERSION=\"${version}\" ${INCLUDES}
 LDFLAGS = ${LIBS}
