7985a66c1f18874145e901c398263dbfe34795f1
Ramil Farkhshatov <ramil@gmx.co.uk>
2010-10-27 19:11:14 +0400

    Minor formatting in makefiles.

--- config.mk
+++ config.mk
@@ -1,17 +1,17 @@
 prefix = /usr/local
-bindir = ${prefix}/bin
-mandir = ${prefix}/share/man
-man1dir = ${mandir}/man1
+bindir = $(prefix)/bin
+mandir = $(prefix)/share/man
+man1dir = $(mandir)/man1
 
 destdir = 
 version = 0.2
 
-INCLUDES = ${shell pkg-config --cflags iksemel} \
-           ${shell pkg-config --cflags gnutls}
+inc = $(shell pkg-config --cflags iksemel) \
+      $(shell pkg-config --cflags gnutls)
 
-LIBS = -lutil \
-       ${shell pkg-config --libs iksemel} \
-       ${shell pkg-config --libs gnutls} \
+libs = -lutil \
+       $(shell pkg-config --libs iksemel) \
+       $(shell pkg-config --libs gnutls)
 
-CFLAGS = -O0 -g -Wall -DVERSION=\"${version}\" ${INCLUDES}
-LDFLAGS = ${LIBS}
+CFLAGS = -O0 -g -Wall -DVERSION=\"$(version)\" $(inc)
+LDFLAGS = $(libs)
